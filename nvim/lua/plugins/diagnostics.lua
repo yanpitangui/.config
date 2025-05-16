12faf9877d664175f92d2a661af50ff7fb6ad9a8
Yan Pitangui <yan.pitangui@nubank.com.br>
2025-05-16 10:56:48 -0300

    fix diagnostics

--- nvim/lua/plugins/diagnostics.lua
+++ nvim/lua/plugins/diagnostics.lua
@@ -1,9 +1,12 @@
 return {
+  {
+
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "LspAttach", -- Or `VeryLazy`
+    enabled = true,
     priority = 1000, -- needs to be loaded in first
     config = function()
-        require('tiny-inline-diagnostic').setup()
-        vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-    end
+      require("tiny-inline-diagnostic").setup()
+    end,
+  },
 }
